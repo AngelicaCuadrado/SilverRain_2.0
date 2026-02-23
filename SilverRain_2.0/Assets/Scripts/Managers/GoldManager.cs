035208c0715e9c40c876e11e8fd56090321502b8
SXY200010 <156553242+SXY200010@users.noreply.github.com>
2026-02-23 10:39:58 -0500

    Upgrade store

--- SilverRain_2.0/Assets/Scripts/Managers/GoldManager.cs
+++ SilverRain_2.0/Assets/Scripts/Managers/GoldManager.cs
@@ -4,22 +4,63 @@ using UnityEngine.Events;
 public class GoldManager : MonoBehaviour
 {
     public static GoldManager Instance { get; private set; }
-    private float currentGold;
+
+    [Header("Gold Data")]
+    [SerializeField] private float currentGold;
     private float spentGold;
-    public static UnityEvent OnGoldChanged;
+
+    public float CurrentGold => currentGold;
+    public float SpentGold => spentGold;
+
+    public static UnityEvent OnGoldChanged = new UnityEvent();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void GainGold(float amount)
     {
-
+        if (amount <= 0) return;
+        currentGold += amount;
+        OnGoldChanged?.Invoke();
     }
 
     public bool SpendGold(float amount)
     {
-        return true;
+        if (amount <= 0) return true;
+
+        if (currentGold >= amount)
+        {
+            currentGold -= amount;
+            spentGold += amount; 
+            OnGoldChanged?.Invoke();
+            return true;
+        }
+
+        return false; 
     }
 
     public void ConvertToGold(float amount, float multiplier)
     {
+        GainGold(amount * multiplier);
+    }
 
+    public void RefundAllSpentGold()
+    {
+        if (spentGold > 0)
+        {
+            currentGold += spentGold;
+            spentGold = 0; 
+            OnGoldChanged?.Invoke();
+        }
     }
 }
