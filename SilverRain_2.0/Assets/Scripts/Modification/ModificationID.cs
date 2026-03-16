fa657f089f64539777048070d058a8ff9e5280f9
SXY200010 <156553242+SXY200010@users.noreply.github.com>
2026-03-16 10:49:37 -0400

    more mod

--- SilverRain_2.0/Assets/Scripts/Modification/ModificationID.cs
+++ SilverRain_2.0/Assets/Scripts/Modification/ModificationID.cs
@@ -2,7 +2,23 @@ using UnityEngine;
 
 public enum ModificationID
 {
+    //Chip Damage Build
     LightMetal,
+    RapidFire,
+
     ExplodingBullets,
-    Frenzy
+
+    //Low HP BD
+    Frenzy,
+    DesperatePower,
+    DesperationArmor,
+    SurvivalInstinct,
+    PanicRush,
+
+    //Full HP Build
+    PerfectForm,
+    StableEnergy,
+    VitalRegeneration,
+    CombatRecovery,
+    PrecisionShot
 }
