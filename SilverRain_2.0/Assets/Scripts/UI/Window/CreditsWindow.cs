3a5316f517ec43a92ffc937ae7af07f9c1474b5a
SXY200010 <156553242+SXY200010@users.noreply.github.com>
2026-04-19 20:30:41 -0400

    debug

--- SilverRain_2.0/Assets/Scripts/UI/Window/CreditsWindow.cs
+++ SilverRain_2.0/Assets/Scripts/UI/Window/CreditsWindow.cs
@@ -4,15 +4,24 @@ using UnityEngine.UI;
 public class CreditsWindow : UIWindow
 {
     [SerializeField] private Button backButton;
+    [SerializeField] private CreditsManager creditsManager;
+
+    private void Awake()
+    {
+        if (creditsManager == null)
+            creditsManager = GetComponentInChildren<CreditsManager>(true);
+    }
     
     public override void OnPushed()
     {
         BindUIEvents();
+        creditsManager?.PlayTimeline();
     }
 
     public override void OnPopped()
     {
         UnbindUIEvents();
+        creditsManager?.StopTimeline();
     }
     
     private void BindUIEvents()
